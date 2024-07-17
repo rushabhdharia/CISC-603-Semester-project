5111f52e71f37621f6037bf648d9f341ecd82a49
rushabhdharia <rushabh.dharia@gmail.com>
2024-07-17 03:36:16 -0400

    Added DFA Code

--- CISC603Project/DFA.cs
+++ CISC603Project/DFA.cs
@@ -1,20 +1,59 @@
 ﻿namespace DFA
 {
-    public class Node
+    public class State(char value)
     {
-        public char value { get; set; }
+        public char Value = value;
+
+        public List<Link> Links = [];
+
+        public void AddLink(Link link)
+        {
+            Links.Add(link);
+        }
     }
 
-    public class Link
+    public class Link(State fromState, char inputAlphabet, State toState)
     {
+        public State FromState = fromState;
+        public char InputAlphabet = inputAlphabet;
+        public State ToState = toState;
 
     }
 
-    public class DFA
+    public class DFA(State initialState, List<State> internalStates, List<State> finalStates)
     {
-        public DFA()
-        { }
+        public State InitialState = initialState;
 
+        public List<State> InternalStates = internalStates;
 
+        public List<State> FinalStates = finalStates;
+
+        private static State? GetNextState(State currentState, char inputAlphabet)
+        {
+
+            foreach (Link link in currentState.Links)
+            {
+                if (link.InputAlphabet == inputAlphabet)
+                {
+                    return link.ToState;
+                }
+            }
+
+            return null;
+        }
+
+        public bool IsAccepting(String inputString)
+        {
+            State currentState = InitialState;
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                currentState = GetNextState(currentState, inputString[i]);
+                if (currentState == null)
+                {
+                    return false;
+                }
+            }
+            return FinalStates.Contains(currentState);
+        }
     }
 }
